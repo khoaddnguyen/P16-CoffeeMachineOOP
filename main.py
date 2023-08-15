cb9f2ae0060d8c032b81e3bc1e026139b6d85c1b
kwa_laa <khoaddnguyen@gmail.com>
2023-08-15 14:42:36 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+# 1 - Print report
+# 2 - Check resource sufficient?
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
+# 3 - Process coins
+# 4 - Check transaction successful?
+# 5 - Make coffee
